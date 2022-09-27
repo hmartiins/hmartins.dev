c4151eb11453cb316537ba8704a55407f6bb0050
hmartiins <hmartins224@gmail.com>
2022-09-27 16:32:36 -0300

    Apply TypeWriterEffect in main

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,20 +1,38 @@
+import TypeWriterEffect from 'typewriter-effect';
+
 import { AiOutlineMail } from 'react-icons/ai';
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
 export default function Main() {
+  const stringsTypeWriter = [
+    'Henrique Martins', 
+    'Santista', 
+    'Desenvolvedor Front-End',
+    'Contribuidor Open-Source'
+  ];
+  
   return (
     <main className="w-full h-screen text-center">
       <div className="max-w-[1024px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
+  
           <p className="uppercase text-sm tracking-widest text-gray-600">
             Moldando o Futuro através de linhas de código
           </p>
           
           <h1 className="py-4 text-gray-700">
-            Olá, eu sou <span className="text-[#0070f3]">Henrique</span>
-          </h1>
-          <h1 className="text-gray-700">
-            Desenvolvedor FrontEnd
+            Olá, eu sou 
+            <span className="text-[#0070f3]">
+              <TypeWriterEffect 
+                options={{
+                  strings: stringsTypeWriter,
+                  autoStart: true,
+                  loop: true,
+                  deleteSpeed: 25,
+                  cursor: '_'
+               }}
+              />
+            </span>
           </h1>
           
           <p className="py-5 text-gray-600 max-w-[70%] m-auto">
